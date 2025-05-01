ef2d5b6ce8a8fe1a64f6e8022616a2c1b44fcb9b
jonas LE DUFF <jonasleduff14@gmail.com>
2025-05-01 17:13:59 +0200

    Classe ValidateurVariables finie et testee + variables retirées du SimulateurV2

---
+++ src/main/java/com/kerware/simulateurreusine/ValidateurVariables.java
@@ -0,0 +1,36 @@
+package com.kerware.simulateurreusine;
+
+import com.kerware.SituationFamiliale;
+
+public class ValidateurVariables {
+    public static void checkVariables(int revNetDecl1, int revNetDecl2, SituationFamiliale sitFam, int nbEnfants,
+                                             int nbEnfantsHandicapes, boolean parentIsol) throws IllegalArgumentException{
+        if ( revNetDecl1  < 0 || revNetDecl2 < 0 ) {
+            throw new IllegalArgumentException("Le revenu net ne peut pas être négatif");
+        }
+
+        if ( nbEnfants < 0 ) {
+            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être négatif");
+        }
+
+        if ( nbEnfantsHandicapes < 0 ) {
+            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être négatif");
+        }
+
+        if ( sitFam == null ) {
+            throw new IllegalArgumentException("La situation familiale ne peut pas être null");
+        }
+
+        if ( nbEnfantsHandicapes > nbEnfants ) {
+            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être supérieur au nombre d'enfants");
+        }
+
+        if ( nbEnfants > 7 ) {
+            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être supérieur à 7");
+        }
+
+        if ( parentIsol && ( sitFam == SituationFamiliale.MARIE || sitFam == SituationFamiliale.PACSE ) ) {
+            throw new IllegalArgumentException("Un parent isolé ne peut pas être marié ou pacsé");
+        }
+    }
+}
